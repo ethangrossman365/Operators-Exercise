7f5f971fd1945f25ad0c01cdce5fc8f750b07ca3
ethan grossman <ethangrossman3@gmail.com>
2023-02-28 13:57:44 -0600

    operator commit

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,14 +1,40 @@
-﻿namespace OperatorExercise
+﻿using System.Formats.Asn1;
+using System.Security.Cryptography.X509Certificates;
+using System;
+namespace OperatorExercise
 {
     public class Program
     {
+
+
+        public static double Area(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
         static void Main(string[] args)
         {
+           
+
             int a = 17;
             int b = 4;
             int quotient = a / b;
             int remainder = a % b;
-            if (a == 17 && b ==4) {Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}"); }
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
+
+            }
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"The area of the circle with radius {radius} is {Area(radius)}");
+      
         }
     }
 }
+
+
+
+
+
